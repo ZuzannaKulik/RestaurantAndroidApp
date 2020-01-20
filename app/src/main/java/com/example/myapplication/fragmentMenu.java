9180601d57bdfd1ca00efd26c16a1d95d4e64bf9
Zuzanna Kulik <zuzanna.kulik@automotivedev.pl>
2020-01-20 17:49:44 +0100

    on click card move to info

--- app/src/main/java/com/example/myapplication/fragmentMenu.java
+++ app/src/main/java/com/example/myapplication/fragmentMenu.java
@@ -1,22 +1,80 @@
 package com.example.myapplication;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 
 public class fragmentMenu extends Fragment {
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment_menu, container, false);
+        GridLayout mainGrid;
+        mainGrid = (GridLayout) view.findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_menu, container, false);
+        return view;
+    }
+
+   // GridLayout mainGrid;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+//
+//        //Set Event
+//        setSingleEvent(mainGrid);
+//        //setToggleEvent(mainGrid);
+//    }
+
+//    private void setToggleEvent(GridLayout mainGrid) {
+//        //Loop all child item of Main Grid
+//        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+//            //You can see , all child item is CardView , so we just cast object to CardView
+//            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+//            cardView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+//                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+//
+//                    } else {
+//                        //Change background color
+//                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+//                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    ((MainActivity)view.getContext()).getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,new fragment_informacje()).commit();
+
+                }
+            });
+        }
     }
 
 }
